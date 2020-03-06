72797b29fd894eb9b498efe7d4c3e8922591a6fd
Chytrvs <marcinchytrzynski@gmail.com>
2020-03-06 18:49:06 +0100

    Add api connection data storage

---
+++ WeatherAggregator/ApiConnectionData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAggregator
+{
+    /// <summary>
+    /// Stores API keys and connection strings for different weather data providers
+    /// </summary>
+    public static class ApiConnectionData
+    {
+        /// <summary>
+        /// Api key used to connect to OpenWeather API
+        /// <see cref="https://openweathermap.org/appid"/>
+        /// </summary>
+        public const string OpenWeatherApiKey = "YOUR_API_KEY";
+        /// <summary>
+        /// Api key used to connect to WeatherStack API
+        /// <see cref="https://weatherstack.com/signup/free"/>
+        /// </summary>
+        public const string WeatherStackApiKey = "YOUR_API_KEY";
+        /// <summary>
+        /// OpenWeather connection string used to get current weather
+        /// </summary>
+        public const string OpenWeatherCurrentWeatherConnectionString = "http://api.openweathermap.org/data/2.5/weather";
+        /// <summary>
+        /// WeatherStack connection string used to get current weather
+        /// </summary>
+        public const string WeatherStackCurrentWeatherConnectionString = "http://api.weatherstack.com/current";
+
+    }
+}
