1fe3a577e665659186c8bb3ecba9e6472f038e7e
Chytrvs <marcinchytrzynski@gmail.com>
2020-03-06 18:36:22 +0100

    Add data models describing forecasts

---
+++ WeatherAggregator/Data/Coordinates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAggregator.Data
+{
+    /// <summary>
+    /// Struct describing coordinates, containst Latitude and Longitutde
+    /// </summary>
+    public struct Coordinates
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ WeatherAggregator/Data/Models/ForecastModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WeatherAggregator.Data.Models
+{
+    /// <summary>
+    /// Contains data that describes weather at given time
+    /// </summary>
+    class ForecastModel
+    {
+        /// <summary>
+        /// Requested location
+        /// </summary>
+        public Coordinates Location { get; set; }
+        /// <summary>
+        /// Date that is covered by this forecast
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Temperature at requested location
+        /// </summary>
+        public double Temperature { get; set; }
+        /// <summary>
+        /// Weather code describing weather conditions at requested location
+        /// <see cref="https://openweathermap.org/weather-conditions"/>
+        /// </summary>
+        public string WeatherCode { get; set; }
+        /// <summary>
+        /// Forecast provider used to get this forecast
+        /// </summary>
+        public ForecastSourceType SourceType { get; set; }
+    }
+}
