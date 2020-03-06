136e40339f163bbb5cf45e8eb686de2670fb66cc
Chytrvs <marcinchytrzynski@gmail.com>
2020-03-06 16:39:25 +0100

    Add .Net Core dependency injection

--- WeatherAggregator/App.xaml.cs
+++ WeatherAggregator/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,23 @@ namespace WeatherAggregator
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider _serviceProvider;
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
+
 }
