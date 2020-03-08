9528cabd8f5f0aa61778f87570a65885463662cd
Chytrvs <marcinchytrzynski@gmail.com>
2020-03-08 23:07:36 +0100

    Implement downloading current weather from WeatherStack

--- WeatherAggregator/Services/WeatherStackForecastDownloader.cs
+++ WeatherAggregator/Services/WeatherStackForecastDownloader.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WeatherAggregator.Data;
 using WeatherAggregator.Data.Models;
 
@@ -9,15 +14,68 @@ namespace WeatherAggregator.Services
 {
     class WeatherStackForecastDownloader : IForecastDownloader
     {
+        #region Private Fields
+        private readonly HttpClient httpClient;
+        private string connectionString;
+        private string apiKey;
+        #endregion
+        #region Constructor
+        public WeatherStackForecastDownloader(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            connectionString = ApiConnectionData.WeatherStackCurrentWeatherConnectionString;
+            apiKey = ApiConnectionData.WeatherStackApiKey;
+        }
+        #endregion
+        #region Private Methods
+        /// <summary>
+        /// Create apropriate connection string to get current weather
+        /// String pattern
+        /// http://api.weatherstack.com/current?access_key={apikey}&query={latitude},{longitude}
+        /// </summary>
+        /// <param name="coordinates"></param>
+        /// <returns></returns>
+        private string ComposeCurrentWeatherRequestString(Coordinates coordinates)
+        {
+            return $"{connectionString}?access_key={apiKey}&query={coordinates.Latitude},{coordinates.Longitude}";
+        }
+
+        #endregion
+        #region Public Methods
         /// <summary>
         /// Downloads current weather state at given coordinates
         /// </summary>
         /// <param name="coordinates"><see cref="Coordinates"></see>/></param>
         /// <returns>Returns deserialized ForecastModel object</returns>
-        public Task<ForecastModel> GetForecast(Coordinates coordinates)
+        public async Task<ForecastModel> GetForecast(Coordinates coordinates)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string requestString = ComposeCurrentWeatherRequestString(coordinates);
+                //get data from api
+                HttpResponseMessage responseMessage = await httpClient.GetAsync(requestString);
+                //check if request is successful, otherwise throw exception
+                responseMessage.EnsureSuccessStatusCode();
+                //extract data from response
+                string responseString = await responseMessage.Content.ReadAsStringAsync();
+                //deserialize response to JArray
+                var response = JsonConvert.DeserializeObject<JToken>(responseString);
+                if (!(response is JArray) && response["error"] != null)
+                {
+                    throw new Exception(response["error"].Last.First.ToString());
+                }
+                //response JArray to target type
+                var deserializedResponse= response.ToObject<WeatherStackDeserializer>();
+                //create and return forecast model with forecast data
+                return new ForecastModel() { Date=DateTime.Today, Location=coordinates, SourceType=ForecastSourceType.WeatherStack, Temperature=deserializedResponse.Current.Temperature, WeatherCode=deserializedResponse.Current.WeatherCode.ToString() };
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Downloading forecast from WeatherStack failed, an exception has occured: {e.Message }");
+            }
+            return null;
         }
+        #endregion
         /// <summary>
         /// Downloads forecast for a given date at given coordinates
         /// </summary>
